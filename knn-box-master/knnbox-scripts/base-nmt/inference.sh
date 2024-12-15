db0247476c3b296ac84c4d15d51eb0b9b6b57bc7
Gadfly <133796091+FeliceRivarez@users.noreply.github.com>
2024-12-15 20:36:21 +0800

    train & bugfix

--- knn-box-master/knnbox-scripts/base-nmt/inference.sh
+++ knn-box-master/knnbox-scripts/base-nmt/inference.sh
@@ -8,14 +8,20 @@
 export OMP_WAIT_POLICY=PASSIVE
 
 PROJECT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../..
-DATA_PATH=$PROJECT_PATH/data-bin/it
-BASE_MODEL=$PROJECT_PATH/pretrain-models/wmt19.de-en/wmt19.de-en.ffn8192.pt
+# DATA_PATH=$PROJECT_PATH/old/it
+# BASE_MODEL=$PROJECT_PATH/pretrain-models/wmt19.de-en/wmt19.de-en.ffn8192.pt
+
+DATA_PATH=/root/dataset/data-bin/
+# BASE_MODEL=$PROJECT_PATH/newTrain/checkpoint_last.pt
+
+BASE_MODEL=/root/autodl-tmp/selective_distillation-main/backup/checkpoint_best.pt
+
 
 CUDA_VISIBLE_DEVICES=0 python $PROJECT_PATH/fairseq_cli/generate.py $DATA_PATH \
 --task translation \
 --path $BASE_MODEL \
 --dataset-impl mmap \
---beam 4 --lenpen 0.6 --max-len-a 1.2 --max-len-b 10 --source-lang de --target-lang en \
+--beam 4 --lenpen 0.6 --max-len-a 1.2 --max-len-b 10 --source-lang en --target-lang de \
 --gen-subset test \
 --model-overrides "{'eval_bleu': False, 'required_seq_len_multiple':1, 'load_alignments': False}" \
 --max-tokens 2048 \
