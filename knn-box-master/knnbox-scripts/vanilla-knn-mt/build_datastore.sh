79c84aa782387f05e6c91a791b3032de8d816c6f
Gadfly <133796091+FeliceRivarez@users.noreply.github.com>
2024-12-15 20:44:14 +0800

    vanilla knn

--- knn-box-master/knnbox-scripts/vanilla-knn-mt/build_datastore.sh
+++ knn-box-master/knnbox-scripts/vanilla-knn-mt/build_datastore.sh
@@ -7,9 +7,28 @@
 export OMP_WAIT_POLICY=PASSIVE
 
 PROJECT_PATH=$( cd -- "$( dirname -- "$ BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../..
-BASE_MODEL=$PROJECT_PATH/pretrain-models/wmt19.de-en/wmt19.de-en.ffn8192.pt
-DATA_PATH=$PROJECT_PATH/data-bin/it
+# BASE_MODEL=$PROJECT_PATH/pretrain-models/wmt19.de-en/wmt19.de-en.ffn8192.pt
+# DATA_PATH=$PROJECT_PATH/data-bin/it
+DATA_PATH=/root/dataset/data-bin/
+# BASE_MODEL=$PROJECT_PATH/newTrain/checkpoint_last.pt
+BASE_MODEL=/root/autodl-tmp/selective_distillation-main/backup/checkpoint_best.pt
+
 DATASTORE_SAVE_PATH=$PROJECT_PATH/datastore/vanilla/it
+
+
+# CUDA_VISIBLE_DEVICES=0 python $PROJECT_PATH/knnbox-scripts/common/validate.py $DATA_PATH \
+# --task translation \
+# --path $BASE_MODEL \
+# --model-overrides "{'eval_bleu': False, 'required_seq_len_multiple':1, 'load_alignments': False}" \
+# --dataset-impl mmap \
+# --valid-subset train \
+# --skip-invalid-size-inputs-valid-test \
+# --max-tokens 4096 \
+# --bpe fastbpe \
+# --user-dir $PROJECT_PATH/knnbox/models \
+# --arch vanilla_knn_mt@transformer_wmt19_de_en \
+# --knn-mode build_datastore \
+# --knn-datastore-path $DATASTORE_SAVE_PATH \
 
 
 CUDA_VISIBLE_DEVICES=0 python $PROJECT_PATH/knnbox-scripts/common/validate.py $DATA_PATH \
@@ -22,6 +41,6 @@ CUDA_VISIBLE_DEVICES=0 python $PROJECT_PATH/knnbox-scripts/common/validate.py $D
 --max-tokens 4096 \
 --bpe fastbpe \
 --user-dir $PROJECT_PATH/knnbox/models \
---arch vanilla_knn_mt@transformer_wmt19_de_en \
+--arch vanilla_knn_mt@transformer \
 --knn-mode build_datastore \
 --knn-datastore-path $DATASTORE_SAVE_PATH \
--- knn-box-master/knnbox-scripts/vanilla-knn-mt/inference.sh
+++ knn-box-master/knnbox-scripts/vanilla-knn-mt/inference.sh
@@ -7,28 +7,55 @@
 export OMP_WAIT_POLICY=PASSIVE
 
 PROJECT_PATH=$( cd -- "$( dirname -- "$ BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../..
-BASE_MODEL=$PROJECT_PATH/pretrain-models/wmt19.de-en/wmt19.de-en.ffn8192.pt
-DATA_PATH=$PROJECT_PATH/data-bin/it
+# BASE_MODEL=$PROJECT_PATH/pretrain-models/wmt19.de-en/wmt19.de-en.ffn8192.pt
+# DATA_PATH=$PROJECT_PATH/data-bin/it
+
+# DATA_PATH=/root/data-bin
+
 DATASTORE_LOAD_PATH=$PROJECT_PATH/datastore/vanilla/it
+# BASE_MODEL=/root/autodl-tmp/knn-box-master/newTrain/checkpoint_best.pt
+DATA_PATH=/root/dataset/data-bin/
+# BASE_MODEL=$PROJECT_PATH/newTrain/checkpoint_last.pt
+
+# BASE_MODEL=$PROJECT_PATH/newTrain/checkpoint_last.pt
+BASE_MODEL=/root/autodl-tmp/selective_distillation-main/backup/checkpoint_best.pt
+
+
+
+
+# CUDA_VISIBLE_DEVICES=0 python $PROJECT_PATH/knnbox-scripts/common/generate.py $DATA_PATH \
+# --task translation \
+# --path $BASE_MODEL \
+# --dataset-impl mmap \
+# --beam 4 --lenpen 0.6 --max-len-a 1.2 --max-len-b 10 --source-lang de --target-lang en \
+# --gen-subset test \
+# --max-tokens 2048 \
+# --scoring sacrebleu \
+# --tokenizer moses \
+# --remove-bpe \
+# --user-dir $PROJECT_PATH/knnbox/models \
+# --arch vanilla_knn_mt@transformer_wmt19_de_en \
+# --knn-mode inference \
+# --knn-datastore-path $DATASTORE_LOAD_PATH \
+# --knn-k 8 \
+# --knn-lambda 0.7 \
+# --knn-temperature 10.0 \
 
 
 CUDA_VISIBLE_DEVICES=0 python $PROJECT_PATH/knnbox-scripts/common/generate.py $DATA_PATH \
 --task translation \
 --path $BASE_MODEL \
 --dataset-impl mmap \
---beam 4 --lenpen 0.6 --max-len-a 1.2 --max-len-b 10 --source-lang de --target-lang en \
+--beam 4 --lenpen 0.6 --max-len-a 1.2 --max-len-b 10 --source-lang en --target-lang de \
 --gen-subset test \
 --max-tokens 2048 \
 --scoring sacrebleu \
 --tokenizer moses \
 --remove-bpe \
 --user-dir $PROJECT_PATH/knnbox/models \
---arch vanilla_knn_mt@transformer_wmt19_de_en \
+--arch vanilla_knn_mt@transformer \
 --knn-mode inference \
 --knn-datastore-path $DATASTORE_LOAD_PATH \
 --knn-k 8 \
---knn-lambda 0.7 \
---knn-temperature 10.0 \
-
-
-
+--knn-lambda 0.4 \
+--knn-temperature 100.0 \
